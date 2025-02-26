962e4d5378303591c4f5cd2ac7d9352b013333ef
Arnavnemade1 <arnavnemade1@gmail.com>
2025-02-26 13:51:42 -0600

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TiwanakuSite from './TiwanakuSite';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <TiwanakuSite />
+    </div>
+  );
+}
+
+export default App;
