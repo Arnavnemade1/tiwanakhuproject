dcdb0dbbe68efb950b015d7aefa0230153ea1284
Arnavnemade1 <arnavnemade1@gmail.com>
2025-03-05 10:55:22 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -9,3 +8,27 @@ root.render(
     <App />
   </React.StrictMode>
 );
+```
+
+Make sure you have a `public/index.html` file:
+
+<antArtifact identifier="index-html" type="application/vnd.ant.code" language="html" title="public/index.html">
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Tiwanakhu Culture Project</title>
+  </head>
+  <body>
+    <div id="root"></div>
+  </body>
+</html>
+```
+
+Then run:
+```bash
+npm start
+```
+
+This should resolve any startup issues. Would you like me to help you develop the full website for the Tiwanakhu culture project?
